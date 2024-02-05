810f1f4ba3feded5f7090f1071608f02680c5d59
Paisley R <90116898+Gapva@users.noreply.github.com>
2024-02-04 20:16:24 -0500

    rebrand

--- example/example.py
+++ example/example.py
@@ -1,4 +1,4 @@
-with open("example.sfvc", "r") as chartfile:
+with open("example.slrc", "r") as chartfile:
     chartprocessed = list(dict.fromkeys([line.lower() for line in chartfile.read().splitlines()])) # remove dupes
 
 configidx = chartprocessed.index('-config')
@@ -9,16 +9,17 @@ chartnotes = chartprocessed[chartidx+1:] # +1 to skip the -chart line itself
 
 offset = int(configkeys[0].split("_")[1])
 tempo = int(configkeys[1].split("_")[1])
+columns = int(configkeys[2].split("_")[1])
 
-print(f"CHART INFO\noffset\t{offset}ms\ntempo\t{tempo}bpm\n")
+print(f"CHART INFO\noffset\t{offset}ms\ntempo\t{tempo}bpm\ncolumns\t{columns}\n")
 print("CHART NOTES")
-for note in range(len(chartnotes)):
+for note in range(len(chartnotes) - 1):
     notetype = chartnotes[note].split("_")[0]
     noteinfo = chartnotes[note].split("_")[1].split(":")
     notecol = int(noteinfo[0])
     notetiming = [int(timing) for timing in noteinfo[1].split("-")]
     match notetype:
         case "rg":
-            print(f"{note}\trice grain\tcolumn {notecol}\tat {notetiming[0]}ms")
+            print(f"{note + 1}\trice grain\tcolumn {notecol}\tat {notetiming[0]}ms")
         case _:
-            print(f"{note}\tnoodle strand\tcolumn {notecol}\tfrom {notetiming[0]}ms to {notetiming[1]}ms")
+            print(f"{note + 1}\tnoodle strand\tcolumn {notecol}\tfrom {notetiming[0]}ms to {notetiming[1]}ms")
